ed0ab54164e67516c4a16b100ec4599cb83d84d6
Pyjter <piotr_zuber@interia.pl>
2019-04-09 07:47:23 +0200

    Trying string interpolation feature

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using LanguageFeatures.Models;
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
         public ViewResult Index()
         {
-            return View(new string[] {"C#","Language","Features" });
+            var results = new List<string>();
+
+            foreach (var p in Product.GetProducts())
+            {
+                var name = p?.Name ?? "<none>";
+                var price = p?.Price ?? 0;
+                var relatedName = p?.Related?.Name ?? "<none>";
+                results.Add($"Product: {name}, price: {price}, relation: {relatedName}");
+            }
+
+            return View(results);
         }
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -8,13 +8,15 @@ namespace LanguageFeatures.Models
     public class Product
     {
         public string Name { get; set; }
+        public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
-
+        public Product Related { get; set; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product
             {
                 Name = "Kayak",
+                Category = "Water Craft",
                 Price = 275M
             };
             Product lifeJacket = new Product
@@ -22,6 +24,8 @@ namespace LanguageFeatures.Models
                 Name = "Life jacket",
                 Price = 48.95M
             };
+
+            kayak.Related = lifeJacket;
 
             return new Product[] { kayak, lifeJacket, null };
 
