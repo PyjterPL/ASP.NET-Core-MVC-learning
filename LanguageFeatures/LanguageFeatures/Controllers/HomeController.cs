21b948710975522085dfe1b0ecc33f7ebc4e72ad
Pyjter <piotr_zuber@interia.pl>
2019-04-09 22:08:34 +0200

    Added some example extension methods with pattern matching, filtering and collection/objects initializers. Most of them are in commentary for archive as notes from exercise.

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -8,19 +8,92 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
+        //public ViewResult Index()
+        //{
+        //    var results = new List<string>();
+
+        //    foreach (var p in Product.GetProducts())
+        //    {
+        //        var name = p?.Name ?? "<none>";
+        //        var price = p?.Price ?? 0;
+        //        var relatedName = p?.Related?.Name ?? "<none>";
+        //        results.Add($"Product: {name}, price: {price:C2}, relation: {relatedName}");
+        //    }
+
+        //    return View(results);
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    Dictionary<string, Product> products = new Dictionary<string, Product>
+        //    {
+        //        { "Kayak", new Product { Name = "Kayak", Price = 275M } },
+        //        { "Lifejacket", new Product { Name = "Life jacket", Price = 48.95M } }
+        //    };
+        //    return View(products.Keys);
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    var data = new object[] { 275M, 29.95M, "Apples", "Oranges", 100, 10 };
+        //    var total = 0M;
+        //    foreach (var item in data)
+        //    {
+        //        if (item is decimal d)
+        //        {
+        //            total += d;
+        //        }
+        //    }
+        //    return View("Index", new string[] { $"Total: {total:C2}" });
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    Dictionary<string, Product> products = new Dictionary<string, Product>
+        //    {
+        //        ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+        //        ["Lifejacket"] = new Product { Name = "Life jacket", Price = 48.95M }
+        //    };
+        //    return View(products.Keys);
+        //}
+
+        //public ViewResult Index()
+        //{
+        //    var data = new object[] { 275M, 29.95M, "Apples", "Oranges", 100, 10 };
+        //    var total = 0M;
+        //    foreach (var item in data)
+        //    {
+        //        switch (item)
+        //        {
+        //            case decimal decimalValue:
+        //                total += decimalValue;
+        //                break;
+        //            case int intValue when intValue > 50:
+        //                total += intValue;
+        //                break;
+        //        }
+        //    }
+        //    return View("Index", new string[] { $"Total: {total:C2}" });
+        //}
+
         public ViewResult Index()
         {
-            var results = new List<string>();
+            var cart = new ShoppingCart { Products = Product.GetProducts() };
 
-            foreach (var p in Product.GetProducts())
+            Product[] productArray =
             {
-                var name = p?.Name ?? "<none>";
-                var price = p?.Price ?? 0;
-                var relatedName = p?.Related?.Name ?? "<none>";
-                results.Add($"Product: {name}, price: {price}, relation: {relatedName}");
-            }
+                new Product{Name = "Kayak", Price= 275M},
+                new Product{Name = "Lifejacket", Price = 48.95M},
+                new Product{Name = "Football", Price=19.50M},
+                new Product{Name = "Flag", Price = 34.95M}
+            };
 
-            return View(results);
+            var cartTotal = cart.TotalPrices();
+            var arrayTotal = productArray.FilterByPrice(20M).TotalPrices();
+            return View(new string[] {
+                $"Total cart cost: {cartTotal:C2}",
+                $"Total array cost: {arrayTotal:C2}"
+            });
         }
     }
 }
--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -7,10 +7,15 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            this.InStock = stock;
+        }
         public string Name { get; set; }
         public string Category { get; set; } = "Watersports";
         public decimal? Price { get; set; }
         public Product Related { get; set; }
+        public bool InStock { get; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product
@@ -19,7 +24,7 @@ namespace LanguageFeatures.Models
                 Category = "Water Craft",
                 Price = 275M
             };
-            Product lifeJacket = new Product
+            Product lifeJacket = new Product(false)
             {
                 Name = "Life jacket",
                 Price = 48.95M
