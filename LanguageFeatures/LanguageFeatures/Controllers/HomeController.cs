57cf7e56bf3005a56efad4e56c4c660883b6c3c7
Pyjter <piotr_zuber@interia.pl>
2019-04-10 22:36:58 +0200

    Using LINQ in HomeController

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -10,28 +10,7 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            var cart = new ShoppingCart { Products = Product.GetProducts() };
-
-            Product[] productArray =
-            {
-                new Product{Name = "Kayak", Price= 275M},
-                new Product{Name = "Lifejacket", Price = 48.95M},
-                new Product{Name = "Football", Price=19.50M},
-                new Product{Name = "Flag", Price = 34.95M}
-            };
-
-            var cartTotal = cart
-                .Filter(p=>(p?.Price ?? 0) >=20)
-                .TotalPrices();
-
-            var arrayTotal = productArray
-                .Filter(p=> (p?.Name?[0] == 'P'))
-                .TotalPrices();
-
-            return View(new string[] {
-                $"Total cart cost: {cartTotal:C2}",
-                $"Total array cost: {arrayTotal:C2}"
-            });
+            return View(Product.GetProducts().Select(p => p?.Name));
         }
     }
 }
