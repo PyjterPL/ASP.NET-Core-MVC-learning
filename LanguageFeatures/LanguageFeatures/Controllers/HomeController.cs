66decded5792de914abbbfbfe9b8af2a8532eba4
Pyjter <piotr_zuber@interia.pl>
2019-04-10 22:38:42 +0200

    Index method as lambda expression in HomeController

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -8,9 +8,7 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index()
-        {
-            return View(Product.GetProducts().Select(p => p?.Name));
-        }
+        public ViewResult Index() =>
+            View(Product.GetProducts().Select(p => p?.Name));
     }
 }
