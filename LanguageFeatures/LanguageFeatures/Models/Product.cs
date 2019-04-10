73b1f35dd9529c29ad94ca6c7063a49df387a58f
Pyjter <piotr_zuber@interia.pl>
2019-04-10 22:40:55 +0200

    Class property as lambda expression in Product.cs

--- LanguageFeatures/LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -16,6 +16,7 @@ namespace LanguageFeatures.Models
         public decimal? Price { get; set; }
         public Product Related { get; set; }
         public bool InStock { get; }
+        public bool NameBeginsWithS => Name?[0] == 'P';
         public static Product[] GetProducts()
         {
             Product kayak = new Product
