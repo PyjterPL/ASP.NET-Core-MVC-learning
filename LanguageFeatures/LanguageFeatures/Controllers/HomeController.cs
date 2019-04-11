a12d9459949cd69b0accbd2dd57d83e6894d473c
Pyjter <piotr_zuber@interia.pl>
2019-04-11 21:06:11 +0200

    Using nameof to get attr names

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -8,10 +8,16 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> Index()
+        public ViewResult Index()
         {
-            var length = await MyAsyncMethods.GetPageLength();
-            return View(new string[] { $"Length is: {length}" });
+            var products = new[] {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p => 
+            $"{nameof(p.Name)}: {p.Name}, {nameof(p.Price)}: {p.Price}"));
         }
     }
 }
